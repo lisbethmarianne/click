e67885daad6efce9534884c3e1a0225db9d9ecf5
Katrin Marschner <katrin.marschner@gmail.com>
2016-03-31 20:11:27 +0200

    connect with mongodb

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 'use strict';
 
 var express = require('express'),
-    routes = require('./app/routes/index.js');
+    routes = require('./app/routes/index.js'),
+    mongo = require('mongodb').MongoClient;
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
-routes(app);
+  if (err) {
+    throw new Error('Database failed to connect!');
+  } else {
+    console.log('MongoDB successfully connected on port 27017.');
+  }
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...');
+  app.use('/public', express.static(process.cwd() + '/public'));
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+  routes(app, db);
+
+  app.listen(3000, function () {
+    console.log('Listening on port 3000...');
+  });
+
 });
