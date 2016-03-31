ecc9645006baec70dded2be66ab6e8a8425e89c7
Katrin Marschner <katrin.marschner@gmail.com>
2016-03-31 19:58:42 +0200

    add simple node server

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000...');
+});
