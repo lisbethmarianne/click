eedf0203a2ca0e317ab698e481da17c166bf665b
Katrin Marschner <katrin.marschner@gmail.com>
2016-04-01 01:33:45 +0200

    use mongoose

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -4,7 +4,7 @@
   var addButton = document.querySelector('.btn-add');
   var deleteButton = document.querySelector('.btn-delete');
   var clickNbr = document.querySelector('#click-nbr');
-  var apiUrl = 'http://localhost:3000/api/clicks';
+  var apiUrl = 'http://localhost:8080/api/clicks';
 
   function ready (fn) {
     if (typeof fn !== 'function') {
@@ -40,13 +40,13 @@
 
   addButton.addEventListener('click', function () {
     ajaxRequest('POST', apiUrl, function () {
-       ajaxRequest('GET', apiUrl, updateClickCount)
+      ajaxRequest('GET', apiUrl, updateClickCount)
     });
   }, false);
 
   deleteButton.addEventListener('click', function () {
     ajaxRequest('DELETE', apiUrl, function () {
-       ajaxRequest('GET', apiUrl, updateClickCount);
+      ajaxRequest('GET', apiUrl, updateClickCount);
     });
   }, false);
 })();
